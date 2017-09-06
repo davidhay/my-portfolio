6fda19988d8589c31ee3a99f8e5f736f9f74593a
David Hay <david.j.hay@gmail.com>
2017-09-06 13:24:39 +0100

    enabled lambda script to work with CodePipeline which has its own bucket

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,14 +6,25 @@ import mimetypes
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
     sns = boto3.resource('sns')
-    
+    location = {
+        "bucketName" : 'portfoliobuild.ealanta.net',
+        "objectKey"  : 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == 'MyAppBuild':
+                    location = artifact["location"]["s3Location"]
+        
+        print "location is " + str(location)
+        
         topic = sns.Topic('arn:aws:sns:us-east-1:492438986868:deploy-portfolio-topic')
         portfolio_bucket = s3.Bucket('portfolio.ealanta.net')
-        build_bucket = s3.Bucket('portfoliobuild.ealanta.net')
+        build_bucket = s3.Bucket(location["bucketName"])
     
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip) 
+        build_bucket.download_fileobj(location['objectKey'],portfolio_zip) 
     
         with zipfile.ZipFile(portfolio_zip) as myzip:
     	    for nm in myzip.namelist():
